d221a6e1819134c9122eeaf792e5a60ef001f069
Lachlan Campbell <lachlanjc@hey.com>
2024-05-01 22:32:47 -0400

    Add local HTTPS

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    https: true,
+    host: true,
+  },
+  plugins: [react(), mkcert()],
   optimizeDeps: {
     esbuildOptions: {
       target: "esnext",
